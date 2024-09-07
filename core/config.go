8bbe20829c5ea16421eeabe83200b9ecce32dab7
tesh254 <ewachira254@gmail.com>
2024-09-07 21:52:01 +0300

    add feature flag mutation

--- core/config.go
+++ core/config.go
@@ -37,6 +37,7 @@ func (config *Config) updateMigrationsConfig(IsMigrationsFolderCreated bool, isM
 		DbVar:                     prevConfig.DbVar,
 		Version:                   constants.VERSION,
 		HasDBUrl:                  prevConfig.HasDBUrl,
+		FeatureFlags:              prevConfig.FeatureFlags,
 	}
 
 	err := fs.writeJSONToFile(".migraine.config.json", configuration)
@@ -46,16 +47,51 @@ func (config *Config) updateMigrationsConfig(IsMigrationsFolderCreated bool, isM
 	}
 }
 
-func (config *Config) UpdateFeatureFlags() {
+func (config *Config) UpdateFeatureFlags(key string) {
+	var fs FS
+	prevConfig := config.getConfig()
+	featureFlags := prevConfig.FeatureFlags
+	switch key {
+	case "VERIFY_DEPS":
+		featureFlags.VERIFY_DEPS = !!featureFlags.VERIFY_DEPS
+	case "INTROSPECT":
+		featureFlags.INTROSPECT = !!featureFlags.INTROSPECT
+	default:
+		log.Printf(":::config::: | Warning: Unknown feature flag key: %s", key)
+	}
+	configuration := Config{
+		IsMigrationsFolderCreated: prevConfig.IsMigrationsFolderCreated,
+		IsMigrationsTableCreated:  prevConfig.IsMigrationsTableCreated,
+		MigrationsPath:            prevConfig.MigrationsPath,
+		EnvFile:                   prevConfig.EnvFile,
+		DbUrl:                     prevConfig.DbUrl,
+		DbVar:                     prevConfig.DbVar,
+		Version:                   constants.VERSION,
+		HasDBUrl:                  prevConfig.HasDBUrl,
+		FeatureFlags:              featureFlags,
+	}
+	err := fs.writeJSONToFile(".migraine.config.json", configuration)
 
+	if err != nil {
+		log.Fatalf(":::config::: | unable to write to `%s.migraine.config.json%s`\n", utils.BOLD, utils.RESET)
+	}
 }
 
-func (config *Config) GetFeatureFlagByKey() {
-
+func (config *Config) GetFeatureFlagByKey(key string) bool {
+	featureFlags := config.getConfig().FeatureFlags
+	switch key {
+	case "VERIFY_DEPS":
+		return featureFlags.VERIFY_DEPS
+	case "INTROSPECT":
+		return featureFlags.INTROSPECT
+	default:
+		log.Printf(":::config::: | Warning: Unknown feature flag key: %s", key)
+		return false
+	}
 }
 
-func (config *Config) GetFeatureFlags() {
-
+func (config *Config) GetFeatureFlags() FeatureFlags {
+	return config.getConfig().FeatureFlags
 }
 
 func (config *Config) updateEnvConfig(envFile string, dbVar string, dbUrl string, hasDBUrl bool) {
