a1541a82e667aa52a88dc185a5e10b6c7234d61c
tesh254 <ewachira254@gmail.com>
2024-11-21 01:54:16 +0300

    update changelog.md for version 0.0.3

--- constants/constants.go
+++ constants/constants.go
@@ -7,7 +7,7 @@ import (
 	"github.com/tesh254/migraine/utils"
 )
 
-const VERSION string = "v0.0.2-alpha.5"
+const VERSION string = "v0.0.3"
 const CONFIG string = ".migraine.config.json"
 const MIGRATION_CONTENT = `--migraine-up
 
