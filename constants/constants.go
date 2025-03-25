7f8399ee21425a425839afab23b139f74b7845ab
wachira <31078302+tesh254@users.noreply.github.com>
2025-03-25 21:30:23 +0300

    ✨ chore: update version to v0.1.1 in constants.go (#12)

--- constants/constants.go
+++ constants/constants.go
@@ -7,7 +7,7 @@ import (
 	"github.com/tesh254/migraine/utils"
 )
 
-const VERSION string = "v0.1.0"
+const VERSION string = "v0.1.1"
 const MIGRAINE_ASCII = `
 
                /$$                              /$$
