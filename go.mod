173bfed7e7f347672b3e23af4ba60687c2300555
tesh254 <ewachira254@gmail.com>
2025-03-26 16:42:23 +0300

    ✨ MBK-45: add shell command execution functionality

--- go.mod
+++ go.mod
@@ -3,14 +3,14 @@ module github.com/tesh254/migraine
 go 1.21.1
 
 require (
-	github.com/joho/godotenv v1.5.1
-	github.com/lib/pq v1.10.9
+	github.com/dgraph-io/badger/v3 v3.2103.5
+	github.com/spf13/cobra v1.8.1
 )
 
 require (
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
-	github.com/dgraph-io/badger/v3 v3.2103.5 // indirect
+	github.com/creack/pty v1.1.24 // indirect
 	github.com/dgraph-io/ristretto v0.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
@@ -19,12 +19,9 @@ require (
 	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/google/flatbuffers v1.12.1 // indirect
-	github.com/gosimple/slug v1.14.0 // indirect
-	github.com/gosimple/unidecode v1.0.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/klauspost/compress v1.12.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.opencensus.io v0.22.5 // indirect
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
---
+++ run/shell.go
@@ -0,0 +1,81 @@
+package run
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"strings"
+
+	"github.com/creack/pty"
+)
+
+func getDefaultShell() string {
+	if shell := os.Getenv("SHELL"); shell != "" {
+		return shell
+	}
+
+	if currentUser, err := user.Current(); err == nil {
+		file, err := os.Open("/etc/passwd")
+		if err == nil {
+			defer file.Close()
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				fields := strings.Split(scanner.Text(), ":")
+				if len(fields) >= 7 && fields[0] == currentUser.Username {
+					if shell := fields[6]; shell != "" {
+						return shell
+					}
+				}
+			}
+		}
+	}
+
+	return "/bin/sh"
+}
+
+func ExecuteCommand(command string) error {
+	shell := getDefaultShell()
+	shellName := filepath.Base(shell)
+
+	var cmd *exec.Cmd
+	switch shellName {
+	case "bash":
+		cmd = exec.Command(shell, "--login", "-c", command)
+	case "zsh":
+		cmd = exec.Command(shell, "-l", "-c", command)
+	case "fish":
+		cmd = exec.Command(shell, "-l", "-c", command)
+	default:
+		cmd = exec.Command(shell, "-c", command)
+	}
+
+	cmd.Env = os.Environ()
+
+	ptmx, err := pty.Start(cmd)
+	if err != nil {
+		return fmt.Errorf("failed to start command: %v", err)
+	}
+	defer ptmx.Close()
+
+	const (
+		colorGray  = "\033[90m"
+		fontSmall  = "\033[2m"
+		resetCodes = "\033[0m"
+	)
+
+	fmt.Fprint(os.Stdout, colorGray+fontSmall)
+
+	_, err = io.Copy(os.Stdout, ptmx)
+
+	fmt.Fprint(os.Stdout, resetCodes)
+
+	if err != nil {
+		return err
+	}
+
+	return cmd.Wait()
+}
