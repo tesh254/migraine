7cf48aa216292b6b512a17870c3672c1141b2593
wachira <31078302+tesh254@users.noreply.github.com>
2025-04-10 16:14:16 +0300

    mbk-54: stabilize read and write operations (#17)

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -7,7 +7,7 @@ import (
 	"github.com/tesh254/migraine/pkg/utils"
 )
 
-const VERSION string = "v0.1.7"
+const VERSION string = "v1.0.0"
 const MIGRAINE_ASCII = `
 
                /$$                              /$$
